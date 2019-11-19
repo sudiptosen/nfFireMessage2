53a0f8a62dc460f0ef30c00ab06223ff89f1869a
Sudipto Sen <sudip2sen@gmail.com>
2019-11-19 00:16:29 -0500

    first commit

--- app/app.component.js
+++ app/app.component.js
@@ -1,9 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var firebase = require("nativescript-plugin-firebase");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
     }
+    AppComponent.prototype.ngOnInit = function () {
+        firebase.init()
+            .then(function () { console.log("Init good"); })
+            .catch(function (err) { console.log("Init bad"); });
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: "ns-app",
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import * as firebase from "nativescript-plugin-firebase";
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    firebase.init()
+      .then(()=> {console.log(`Init good`);})
+      .catch(err => {console.log(`Init bad`);})
+  }
+}
